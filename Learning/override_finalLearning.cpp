09aa49342b410acccc9e922d6b9d0cb606f3257d
HeLi-Control <1939821708@qq.com>
2023-07-09 22:02:54 +0800

    Added the function bodies.

--- Learning/override_finalLearning.cpp
+++ Learning/override_finalLearning.cpp
@@ -7,31 +7,31 @@ struct Base
 {
 public:
     // 虚函数
-    virtual void Show(int x);
+    virtual void Show(int x){};
 };
 struct Derived : public Base
 {
 public:
     // o 写成了 0，新的虚函数
-    virtual void Sh0w(int x);
+    virtual void Sh0w(int x){};
     // 参数列表不一样，新的虚函数
-    virtual void Show(double x);
+    virtual void Show(double x){};
     // const 属性不一样，新的虚函数
-    virtual void Show(int x) const;
+    virtual void Show(int x) const {};
 #if (GET_ERROR == 1)
     // const 属性不一样，新的虚函数，但是由于出现了 override 关键字所以报错
-    virtual void Show(int x) const override;
+    virtual void Show(int x) const override{};
 #endif
 
     // 新的不能被继承的虚函数
-    virtual void show(void) final;
+    virtual void show(void) final{};
 };
 struct NewDerived final : public Derived
 {
 public:
 #if (GET_ERROR == 1)
     // 尝试重写不能被重写的虚函数，报错
-    virtual void show(void) override;
+    virtual void show(void) override{};
 #endif
     int test;
 };
