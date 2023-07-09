b30b36abdc33f4bdfacdcc5d3230d3efe61a9817
HeLi-Control <1939821708@qq.com>
2023-07-09 22:01:43 +0800

    Added the macro so that the file would not get error while compling.

--- Learning/override_finalLearning.cpp
+++ Learning/override_finalLearning.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// 该宏写 1 会让编译器报错，写 0 会将报错部分注释
+#define GET_ERROR 0
+
 struct Base
 {
 public:
@@ -15,8 +18,10 @@ public:
     virtual void Show(double x);
     // const 属性不一样，新的虚函数
     virtual void Show(int x) const;
+#if (GET_ERROR == 1)
     // const 属性不一样，新的虚函数，但是由于出现了 override 关键字所以报错
     virtual void Show(int x) const override;
+#endif
 
     // 新的不能被继承的虚函数
     virtual void show(void) final;
@@ -24,14 +29,19 @@ public:
 struct NewDerived final : public Derived
 {
 public:
+#if (GET_ERROR == 1)
     // 尝试重写不能被重写的虚函数，报错
     virtual void show(void) override;
+#endif
+    int test;
 };
+#if (GET_ERROR == 1)
 class tryDerive : NewDerived
 {
 public:
     int a;
 };
+#endif
 
 int main(int argc, char **argv)
 {
